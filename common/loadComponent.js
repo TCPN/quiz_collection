4488a92d4399b8a21df79846552b2852f45fe3ee
Yang-En Chen <boyenen@gmail.com>
2026-03-08 21:12:24 +0800

    fix: fix paths of images in header

--- common/loadComponent.js
+++ common/loadComponent.js
@@ -7,5 +7,19 @@ async function loadComponent(el, file) {
   if (!el) {
     throw new Error(`Cannot find the mount point ${el} for ${file}`);
   }
-  el.outerHTML = html;
+  const replacedHTML = html.replaceAll(/src="([^"]+)"/g, (match, p1) => {
+    const newPath = replaceAlias(p1);
+    if (newPath) {
+      return `src="${newPath}"`;
+    }
+    return match;
+  });
+  console.log(replacedHTML);
+  el.outerHTML = replacedHTML;
+}
+
+function replaceAlias(path) {
+  if (path.startsWith('@/')) {
+    return path.replace('@/', '/~r04922053/goodtv/'); // this is for csie static server
+  }
 }
